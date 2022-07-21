5f549413b6a8794ae4306804c8d3f773787960f6
liltousin <sergeypletnev2004@gmail.com>
2022-07-21 18:21:38 +0300

    del data.db

--- data-worker.py
+++ data-worker.py
@@ -1,2 +1,21 @@
 import sqlite3
+from sqlite3 import Error
+
+
+def sql_connection():
+    try:
+        con = sqlite3.connect('data.db')
+        return con
+    except Error:
+        print(Error)
+
+
+def create_table_users(con):
+    cur = con.cursor()
+    cur.execute("")
+
+
+def worker():
+    connection = sql_connection()
+    create_table_users(connection)
 
