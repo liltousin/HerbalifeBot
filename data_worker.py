e16f992aae70c909b9f42cd056c920d4cf52932f
liltousin <sergeypletnev2004@gmail.com>
2022-07-23 22:00:04 +0300

    add table teams and table users generators functions

--- data_worker.py
+++ data_worker.py
@@ -12,10 +12,19 @@ def sql_connection():
 
 def create_table_users(con):
     cur = con.cursor()
-    cur.execute("CREATE TABLE users(telegram_id integer PRIMARY KEY, name text, )")
+    cur.execute("CREATE TABLE IF NOT EXISTS users(telegram_id integer PRIMARY KEY, name text)")
+    con.commit()
+
+
+def create_table_teams(con):
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, member_1_id integer, 
+    member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, member_6_id integer, 
+    member_7_id integer, member_8_id integer)""")
+    con.commit()
 
 
 def worker():
     connection = sql_connection()
     create_table_users(connection)
-
+    create_table_teams(connection)
