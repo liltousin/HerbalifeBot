9a975f1defc3e90b4d56d41ff72001e561109b32
liltousin <sergeypletnev2004@gmail.com>
2022-07-24 17:08:53 +0300

    Changing the structure of project. The database is now a Date class object

--- data_worker.py
+++ data_worker.py
@@ -2,29 +2,27 @@ import sqlite3
 from sqlite3 import Error
 
 
-def sql_connection():
-    try:
-        con = sqlite3.connect('data.db')
-        return con
-    except Error:
-        print(Error)
+class Data:
+    def __init__(self):
+        try:
+            self.con = sqlite3.connect('data.db')
+        except Error:
+            print(Error)
 
+    def create_table_users(self):
+        cur = self.con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS users(telegram_id integer PRIMARY KEY, name text)")
+        self.con.commit()
 
-def create_table_users(con):
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users(telegram_id integer PRIMARY KEY, name text)")
-    con.commit()
-
-
-def create_table_teams(con):
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, member_1_id integer, 
-    member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, member_6_id integer, 
-    member_7_id integer, member_8_id integer)""")
-    con.commit()
+    def create_table_teams(self):
+        cur = self.con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, member_1_id integer, 
+        member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, member_6_id integer, 
+        member_7_id integer, member_8_id integer)""")
+        self.con.commit()
 
 
 def worker():
-    connection = sql_connection()
-    create_table_users(connection)
-    create_table_teams(connection)
+    data = Data()
+    data.create_table_teams()
+    data.create_table_users()
