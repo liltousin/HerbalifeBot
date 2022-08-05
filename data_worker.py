ef09398f83146a412a032d6deaa9e62c9b7c739c
liltousin <sergeypletnev2004@gmail.com>
2022-08-05 22:25:19 +0300

    add decorators and some changes

--- data_worker.py
+++ data_worker.py
@@ -1,30 +1,35 @@
 import sqlite3
 from sqlite3 import Error
 
+cursorObj = type(sqlite3.connect('').cursor())
 
-class Data:
-    def __init__(self):
+
+def db_connect(func):
+    def decorator(*args):
         try:
-            self.db = sqlite3.connect('data.db')
-            cur = self.db.cursor()
-            cur.execute("CREATE TABLE IF NOT EXISTS users(user_id integer PRIMARY KEY, name text)")
-            cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, 
-            member_1_id integer, member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, 
-            member_6_id integer, member_7_id integer, member_8_id integer)""")
-            self.db.commit()
+            db = sqlite3.connect('data.db')
+            cur = db.cursor()
+            func(cur, *args)
+            db.commit()
+            db.close()
         except Error:
             print(Error)
-        finally:
-            self.db.close()
 
-    def user_check(self, user_id):
-        try:
-            self.db = sqlite3.connect('data.db')
-            cur = self.db.cursor()
-            info = cur.execute(f"SELECT * FROM users WHERE user_id = {user_id}")
-        except Error:
-            print(Error)
-        finally:
-            self.db.close()
+    return decorator
+
+
+@db_connect
+def create_db(cur: cursorObj):
+    cur.execute("CREATE TABLE IF NOT EXISTS users(user_id integer PRIMARY KEY, name text)")
+    cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, 
+    member_1_id integer, member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, 
+    member_6_id integer, member_7_id integer, member_8_id integer)""")
+
+
+@db_connect
+def user_check(cur: cursorObj, user_id):
+    cur.execute(f"SELECT * FROM users WHERE user_id = {user_id}")
+    info = cur.fetchall() is None
+    return info
 
 
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from config import TOKEN
+import data_worker as db
 from aiogram import Dispatcher, Bot, executor, types
-from data_worker import Data
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-database = Data()
 
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    database.user_check(message.from_user.id)
-    await message.reply('Поехали')
+    if db.user_check(message.from_user.id):
+        await message.reply('')
+    else:
+
+        await message.reply('Кажется вы не пользовались до этого ботом.\nСкажите как вас зовут:')
 
 
 if __name__ == '__main__':
+    db.create_db()
     executor.start_polling(dp)
