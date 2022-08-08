12f187dcea0b8ac2409fd51cc53c74efcdb08654
liltousin <sergeypletnev2004@gmail.com>
2022-08-08 19:21:13 +0300

    add user firstname and lastname registration

--- data_worker.py
+++ data_worker.py
@@ -9,18 +9,18 @@ def db_connect(func):
         try:
             db = sqlite3.connect('data.db')
             cur = db.cursor()
-            func(cur, *args)
+            data = func(cur, *args)
             db.commit()
             db.close()
+            return data
         except Error:
             print(Error)
-
     return decorator
 
 
 @db_connect
 def create_db(cur: cursorObj):
-    cur.execute("CREATE TABLE IF NOT EXISTS users(user_id integer PRIMARY KEY, name text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS users(user_id integer PRIMARY KEY, firstname text, lastname text)")
     cur.execute("""CREATE TABLE IF NOT EXISTS teams(team_id integer PRIMARY KEY, leader_id integer, 
     member_1_id integer, member_2_id integer, member_3_id integer, member_4_id integer, member_5_id integer, 
     member_6_id integer, member_7_id integer, member_8_id integer)""")
@@ -28,8 +28,20 @@ def create_db(cur: cursorObj):
 
 @db_connect
 def user_check(cur: cursorObj, user_id):
-    cur.execute(f"SELECT * FROM users WHERE user_id = {user_id}")
-    info = cur.fetchall() is None
+    cur.execute(f'SELECT * FROM users WHERE user_id = {user_id}')
+    info = cur.fetchall() != []
     return info
+
+
+@db_connect
+def add_user_data(cur: cursorObj, user_id, data):
+    cur.execute(f"INSERT INTO users VALUES({user_id}, '{data['firstname']}', '{data['lastname']}')")
+
+
+@db_connect
+def get_user_name(cur: cursorObj, user_id):
+    cur.execute(f'SELECT firstname, lastname FROM users WHERE user_id = {user_id}')
+    lastname, firstname = cur.fetchall()[0]
+    return firstname, lastname
 
 
--- main.py
+++ main.py
@@ -2,20 +2,69 @@ from config import TOKEN
 import data_worker as db
 from aiogram import Dispatcher, Bot, executor, types
 from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
+
+class UserRegistration(StatesGroup):
+    waiting_for_firstname = State()
+    waiting_for_lastname = State()
+    waiting_for_confirmation = State()
+
+
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
     if db.user_check(message.from_user.id):
-        await message.reply('')
+        firstname, lastname = db.get_user_name(message.from_user.id)
+        await message.answer(f'Здравствуйте {firstname} {lastname}', reply_markup=types.ReplyKeyboardRemove())
     else:
+        await message.answer('Кажется вы не пользовались до этого ботом.')
+        await message.answer('Введите ваше имя', reply_markup=types.ReplyKeyboardRemove())
+        await UserRegistration.first()
 
-        await message.reply('Кажется вы не пользовались до этого ботом.\nСкажите как вас зовут:')
 
+@dp.message_handler(state=UserRegistration.waiting_for_firstname)
+async def entered_firstname(message: types.Message, state: FSMContext):
+    await state.update_data(firstname=message.text.capitalize())
+    await message.answer('Введите вашу фамилию', reply_markup=types.ReplyKeyboardRemove())
+    await UserRegistration.next()
+
+
+@dp.message_handler(state=UserRegistration.waiting_for_lastname)
+async def entered_lastname(message: types.Message, state: FSMContext):
+    await state.update_data(lastname=message.text.capitalize())
+    user_data = await state.get_data()
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('Да')
+    keyboard.add('Нет')
+    await message.answer(f"{user_data['firstname']} {user_data['lastname']}, все верно?", reply_markup=keyboard)
+    await UserRegistration.next()
+
+
+@dp.message_handler(state=UserRegistration.waiting_for_confirmation)
+async def user_data_validation(message: types.Message, state: FSMContext):
+    if message.text == 'Нет':
+        await UserRegistration.first()
+        await message.answer('Введите ваше имя', reply_markup=types.ReplyKeyboardRemove())
+        await state.reset_data()
+    elif message.text == 'Да':
+        user_data = await state.get_data()
+        db.add_user_data(message.from_user.id, user_data)
+        await message.answer(
+            f"{user_data['firstname']} {user_data['lastname']}, вы успешно зарегистрировались.",
+            reply_markup=types.ReplyKeyboardRemove())
+        await state.finish()
+        firstname, lastname = db.get_user_name(message.from_user.id)
+        await message.answer(f'Здравствуйте {firstname} {lastname}', reply_markup=types.ReplyKeyboardRemove())
+    else:
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('Да')
+        keyboard.add('Нет')
+        await message.answer('Используйте клавиатуру!', reply_markup=keyboard)
 
 if __name__ == '__main__':
     db.create_db()
